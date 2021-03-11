a8e745fcd6124b39ce3a14853d590169445a4c24
mrideout1985 <mrideout1985@gmail.com>
2021-03-11 03:46:57 +0000

    error fix

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-unused-vars */
 import React, { useState, useEffect } from "react";
 import sanityClient from "../../client.js";
 import SanityBlockContent from "@sanity/block-content-to-react";
